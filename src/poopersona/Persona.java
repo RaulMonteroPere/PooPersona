71cba8229ee05a78a53f6cd5c17d7ce7dfdf2bc5
JavierVivar <javiertxu71@hotmail.com>
2020-02-18 23:43:05 +0100

    cambios

--- src/poopersona/Persona.java
+++ src/poopersona/Persona.java
@@ -1,4 +1,3 @@
-
 package poopersona;
 
 
@@ -6,8 +5,14 @@ public class Persona {
     private String nombre;
     private String dni;
     private String direccion;
-    private int edad;   
+    private int edad;
+    private int codigo_postal; 
+    private String ciudad; 
+    private String aficiones; 
 
+    /*
+     * Constructores
+     */
     public Persona(String nombre, String dni, String direccion, int edad) {
         this.nombre = nombre;
         this.dni = dni;
@@ -15,6 +20,20 @@ public class Persona {
         this.edad = edad;
     }
 
+    public Persona(String nombre, String dni, String direccion, int edad, int codigo_postal, String ciudad, String aficiones) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.direccion = direccion;
+        this.edad = edad;
+        this.codigo_postal = codigo_postal; 
+        this.ciudad = ciudad; 
+        this.aficiones = aficiones; 
+    }
+    
+    /*
+     * Getter y Setter
+     */
+    
     public String getNombre() {
         return nombre;
     }
@@ -46,10 +65,41 @@ public class Persona {
     public void setEdad(int edad) {
         this.edad = edad;
     }
-    
-    
-    public String consultaPersona() {
-        return "Persona{" + "nombre=" + nombre + ", dni=" + dni + ", direccion=" + direccion + ", edad=" + edad + '}';
-    } 
 
+    
+    public int getCodigo_postal() {
+		return codigo_postal;
+	}
+
+	public void setCodigo_postal(int codigo_postal) {
+		this.codigo_postal = codigo_postal;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getAficiones() {
+		return aficiones;
+	}
+
+	public void setAficiones(String aficiones) {
+		this.aficiones = aficiones;
+	}
+
+	/*
+	 * Métodos
+	 */
+	
+	public String consultaPersona() {
+        return "Persona{" + "nombre=" + nombre + ", dni=" + dni + ", direccion=" + direccion + ", edad=" + edad + '}';
+    }
+    
+	public String consultaCompletaPersona() {
+		return "Persona{" + "nombre=" + nombre + ", dni=" + dni + ", direccion=" + direccion + ", Código Postal: " + codigo_postal + ", Ciudad:  " + ciudad +  ",  edad=" + edad + ", Aficiones: " + aficiones + '}';
+	}
 }
